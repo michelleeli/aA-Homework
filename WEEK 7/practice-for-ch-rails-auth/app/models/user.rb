8cdb2c7180986e2e2f3091e57b5b494f1d02a71b
Michelle Li <rphmichelle@gmail.com>
2023-06-11 23:54:58 -0400

    W7D1 HW

---
+++ WEEK 7/practice-for-ch-rails-auth/app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank'}
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    # `user&.is_password?(password)` is syntactic sugar for
+    # `user && user.is_password?(password)`
+    user&.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+  
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    # `SecureRandom::urlsafe_base64` does not guarantee uniqueness. Wrapping
+    # this call in a loop ensures that no other user has the generated token.
+    loop do
+      session_token = SecureRandom::urlsafe_base64(16)
+      return session_token unless User.exists?(session_token: session_token)
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+end
